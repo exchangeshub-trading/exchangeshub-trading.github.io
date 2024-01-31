2bd248cb17782724d816006750debc1131c02d85
Ostafii4uk <ostafii4uk@gmail.com>
2024-01-31 11:14:23 +0200

    Updated nextjs.yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: { unoptimized: true },
   output: 'export',
   pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
   reactStrictMode: true,
