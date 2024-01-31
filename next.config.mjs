d454134faa15923e4f34dd14c0fbb70aa5b1afc9
Ostafii4uk <ostafii4uk@gmail.com>
2024-01-31 12:00:57 +0200

    Updated next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { unoptimized: true },
   output: 'export',
+  distDir: 'dist',
   pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
   reactStrictMode: true,
 }
