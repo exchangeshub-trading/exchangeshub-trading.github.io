514cb39afbcd90c4ca877bac108b870a79bfb74f
Mari <mari.philon@gmail.com>
2024-04-03 16:59:18 +0300

    re-write code and fix frame

--- app/components/LaunchSoonSubscribe/LaunchSoonSubscribe.tsx
+++ app/components/LaunchSoonSubscribe/LaunchSoonSubscribe.tsx
@@ -7,15 +7,15 @@ import { FallingLines } from 'react-loader-spinner';
 const LaunchSoonSubscribe: React.FC = () => {
     const [state, handleSubmit] = useForm("xjvnadyq");
     const [submitSuccess, setSubmitSuccess] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        setIsLoading(true);
+        setLoading(true);
 
         await handleSubmit(event);
 
-        setIsLoading(false);
+        setLoading(false);
         setSubmitSuccess(true);
 
         setTimeout(() => {
@@ -23,37 +23,47 @@ const LaunchSoonSubscribe: React.FC = () => {
         }, 3000);
     };
 
-    return (
-        <div className="launch-soon-subscribe-area">
-            {!isLoading && !submitSuccess && (
-                <form
-                    className="subscribe-form"
-                    onSubmit={handleFormSubmit}
-                    action="https://formspree.io/f/xjvnadyq"
-                    method="POST"
-                >
-                    <label htmlFor="email">Enter your email:</label>
-                    <div className="subscribe-form-input">
-                        <input type="email" id="email" name="email" className="form-control" placeholder="your@email.com"/>
-                        <ValidationError
-                            prefix="Email"
-                            field="email"
-                            errors={state.errors}
-                        />
-                        <button type="submit" className="btn-primary" disabled={state.submitting}>Get Started</button>
-                    </div>
-                </form>
-            )}
-            {isLoading && (
+    if (loading) {
+        return (
+            <div className='launch-soon-subscribe-area'>
                 <div className="spinner-container">
                     <FallingLines color="#4fa94d" width="100" visible={true} />
                 </div>
-            )}
-            {submitSuccess && (
-                <p className='succeeded'>Your email has been submitted. You will get a response within 24 hours. Thank you! </p>
-            )}
+            </div>
+        );
+    }
+
+    if (submitSuccess) {
+        return (
+            <div className='launch-soon-subscribe-area'>
+                <p className='succeeded'>Your email has been submitted. You will get a response within 24 hours. Thank you! </p>  
+            </div>
+        );
+    }
+
+    return (
+        <div className="launch-soon-subscribe-area">
+            <form
+                className="subscribe-form"
+                onSubmit={handleFormSubmit}
+                action="https://formspree.io/f/xjvnadyq"
+                method="POST"
+            >
+                <label htmlFor="email">Enter your email:</label>
+                <div className="subscribe-form-input">
+                    <input type="email" id="email" name="email" className="form-control" placeholder="your@email.com"/>
+                    <ValidationError
+                        prefix="Email"
+                        field="email"
+                        errors={state.errors}
+                    />
+                    <button type="submit" className="btn-primary" disabled={state.submitting}>Get Started</button>
+                </div>
+            </form>
         </div>
     );
 }
 
 export default LaunchSoonSubscribe;
+
+
