df8d41ae0e10a6b7e7ded16277b3ab78f70b4fc6
Mari <mari.philon@gmail.com>
2024-04-04 22:16:58 +0300

    add: invisible recapcha

--- app/components/LaunchSoonSubscribe/LaunchSoonSubscribe.tsx
+++ app/components/LaunchSoonSubscribe/LaunchSoonSubscribe.tsx
@@ -1,63 +1,110 @@
 'use client'
 
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 import { FallingLines } from 'react-loader-spinner';
+import ReCAPTCHA from "react-google-recaptcha";
 
 const LaunchSoonSubscribe: React.FC = () => {
-  const [state, handleSubmit] = useForm("xjvnadyq");
-  const [submitSuccess, setSubmitSuccess] = useState(false);
-  const [loading, setLoading] = useState(false);
+    const [state, handleSubmit] = useForm("moqgkrbz");
+    const [submitSuccess, setSubmitSuccess] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [captchaValue, setCaptchaValue] = useState<string | null>(null);
+    const [showCaptcha, setShowCaptcha] = useState(false);
+    const [showGetStartedButton, setShowGetStartedButton] = useState(true);
+    const recaptchaRef = useRef<any>(null);
 
-  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setLoading(true);
+    useEffect(() => {
+        setShowCaptcha(false);
+        setShowGetStartedButton(true);
+    }, []);
 
-    await handleSubmit(event);
+    useEffect(() => {
+        setShowCaptcha(false);
+        setShowGetStartedButton(true);
+    }, [submitSuccess]);
 
-    setLoading(false);
-    setSubmitSuccess(true);
+    const onChange = (value: string | null) => {
+        setCaptchaValue(value);
+    };
 
-    setTimeout(() => {
-      setSubmitSuccess(false);
-    }, 3000);
-  };
+    const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+      
+        setLoading(true);
 
-  return (
-    <div className="launch-soon-subscribe-area">
-      {loading ? (
-        <div className='spinner-container'>
-          <div className="spinner-container">
-            <FallingLines color="#4fa94d" visible={true} />
-          </div>
+        await handleSubmit(event);
+
+        setLoading(false);
+        setSubmitSuccess(true);
+
+        setTimeout(() => {
+            setSubmitSuccess(false);
+        }, 3000);
+    };
+
+    const handleGetStartedClick = () => {
+        setShowCaptcha(true);
+        setShowGetStartedButton(false);
+        if (recaptchaRef.current) {
+            recaptchaRef.current.execute();
+        }
+    };
+
+
+    if (loading) {
+        return (
+            <div className='launch-soon-subscribe-area'>
+                <div className="spinner-container">
+                    <FallingLines color="#4fa94d" visible={true} />
+                </div>
+            </div>
+        );
+    }
+
+    if (submitSuccess) {
+        return (
+            <div className='launch-soon-subscribe-area'>
+                <p className='succeeded'>Your email has been submitted. You will get a response within 24 hours. Thank you! </p>  
+            </div>
+        );
+    }
+
+    return (
+        <div className="launch-soon-subscribe-area">
+            {!loading && !submitSuccess && (
+                <form
+                    className="subscribe-form"
+                    onSubmit={handleFormSubmit}
+                    action="https://formspree.io/f/moqgkrbz"
+                    method="POST"
+                >
+                    <label htmlFor="email">Enter your email:</label>
+                    <div className="subscribe-form-input">
+                        <input type="email" id="email" name="email" className="form-control" placeholder="your@email.com"/>
+                        <ValidationError
+                            prefix="Email"
+                            field="email"
+                            errors={state.errors}
+                        />
+                        <ReCAPTCHA
+                            ref={recaptchaRef}
+                            className='recaptcha'
+                            sitekey="6LctM68pAAAAANrY89BubPskNWd63i7cotYDwixX"
+                            onChange={onChange}
+                            size="invisible"
+                        />
+                        {showGetStartedButton && (
+                            <button type="button" className="btn-primary" disabled={state.submitting} onClick={handleGetStartedClick}>Get Started</button>
+                        )}
+                        {showCaptcha && (
+                            <button type="submit" className="btn-primary" disabled={!captchaValue || state.submitting}>Submit</button>
+                        )}
+                    </div>
+                </form>
+            )}
         </div>
-      ) : submitSuccess ? (
-        <div className='launch-soon-subscribe-area'>
-          <p className='succeeded'>Your email has been submitted. You will get a response within 24 hours. Thank you! </p>
-        </div>
-      ) : (
-        <form
-          className="subscribe-form"
-          onSubmit={handleFormSubmit}
-          action="https://formspree.io/f/xjvnadyq"
-          method="POST"
-        >
-          <label htmlFor="email">Enter your email:</label>
-          <div className="subscribe-form-input">
-                <input type="email" id="email" name="email" className="form-control" placeholder="your@email.com"/>
-                <ValidationError
-                prefix="Email"
-                field="email"
-                errors={state.errors}
-                />
-                <div className="g-recaptcha" data-sitekey="6LfPxa4pAAAAAJTzwAX6BG3kMSc5ajbo4_YmC3ft"></div> 
-                <br/>
-                <button type="submit" className="btn-primary" disabled={state.submitting}>Get Started</button>
-          </div>
-        </form>
-      )}
-    </div>
-  );
+    );
 }
 
 export default LaunchSoonSubscribe;
